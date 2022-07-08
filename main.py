e9a05c9b4c7f9cb73739ecf664c9ea981ca0c69e
Sergey Korneev <sergeykorneev226@gmail.com>
2022-07-08 15:16:29 +0300

    polymorphism

--- main.py
+++ main.py
@@ -241,41 +241,112 @@
 # print(SpiderMan.mro())
 
 
-class Human():
-    def __init__(self, name, gender, height=0, weight=0, hands=2):
-        self.name = name
-        self.gender = gender
-        self.height = height
-        self.weight = weight
-        self.hands = hands
+# class Human():
+#     def __init__(self, name, gender, height=0, weight=0, hands=2):
+#         self.name = name
+#         self.gender = gender
+#         self.height = height
+#         self.weight = weight
+#         self.hands = hands
+#
+# class Spider():
+#     def __init__(self, gender, height=0, weight=0, hands=6):
+#         self.gender = gender
+#         self.height = height
+#         self.weight = weight
+#         self.hands = hands
+#
+#     def webshoot(self):
+#         print('Pew-Pew')
+#
+# class SpiderMan(Human, Spider):
+#     def __init__(self, name, gender):
+#         self.weapons = []
+#         super().__init__(name, gender)
+#
+#     def attack(self):
+#         if 'web' in self.weapons:
+#             super().webshoot()
+#         else:
+#             print('No web')
 
-class Spider():
-    def __init__(self, gender, height=0, weight=0, hands=6):
-        self.gender = gender
-        self.height = height
-        self.weight = weight
-        self.hands = hands
+# peter_parker = SpiderMan('Peter Parker', 'Male')
+# peter_parker.attack()
+#
+# peter_parker.weapons = 'web'
+# peter_parker.attack()
+#
+# print(SpiderMan.mro())
 
-    def webshoot(self):
-        print('Pew-Pew')
 
-class SpiderMan(Human, Spider):
-    def __init__(self, name, gender):
-        self.weapons = []
-        super().__init__(name, gender)
+# class Primate:
+#     def eat(self, food):
+#         print(food)
+#
+# class Man(Primate):
+#     def eat(self, food, cooked=False):
+#         if cooked:
+#             print('cooked', food)
+#         else:
+#             print(food)
 
-    def attack(self):
-        if 'web' in self.weapons:
-            super().webshoot()
-        else:
-            print('No web')
 
-peter_parker = SpiderMan('Peter Parker', 'Male')
-peter_parker.attack()
-
-peter_parker.weapons = 'web'
-peter_parker.attack()
-
-print(SpiderMan.mro())
-
+# class Human():
+#     def __init__(self, name, gender, height=0, weight=0, hands=2):
+#         self.name = name
+#         self.gender = gender
+#         self.height = height
+#         self.weight = weight
+#         self.hands = hands
+#
+#     def move(self):
+#         self.weight -= 0.01
+#
+# class Spider():
+#     def __init__(self, gender, height=0, weight=0, hands=6):
+#         self.gender = gender
+#         self.height = height
+#         self.weight = weight
+#         self.hands = hands
+#
+#     def webshoot(self):
+#         print('Pew-Pew')
+#
+# class SpiderMan(Human, Spider):
+#     def __init__(self, name, gender):
+#         self.weapons = []
+#         super().__init__(name, gender)
+#
+#     def attack(self):
+#         if 'web' in self.weapons:
+#             super().webshoot()
+#         else:
+#             print('No web')
+#
+#     def move(self):
+#         super().webshoot()
+#         super().move()
+#
+#     def __lt__(self, other):
+#         if not isinstance(other, SpiderMan):
+#             print('Not a SpiderMan')
+#         return len(self.weapons) < len(other.weapons)
+#
+#     def __add__(self, weapon):
+#         if not isinstance(weapon, str):
+#             print('Error')
+#         self.weapons.append(weapon)
+#
+# peter_parker = SpiderMan('Peter Parker', 'Male')
+# miles_morales = SpiderMan('Miles_Morales', 'Male')
+# # peter_parker.weapons += 'web_shooter'
+# # print(peter_parker.weapons)
+# # miles_morales.weapons += 'web_shooter', 'electricity'
+# # print(miles_morales.weapons)
+# # print(peter_parker < miles_morales)
+# # print(peter_parker > miles_morales)
+#
+# print(peter_parker.weapons)
+# peter_parker + 'strike'
+# print(peter_parker.weapons)
 
